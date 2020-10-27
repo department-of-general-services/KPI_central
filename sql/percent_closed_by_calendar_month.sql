3075633298a4c13940e63576c45b23dc6b4b6d61
James Trimarco <james.trimarco@gmail.com>
2020-10-27 13:22:05 -0400

    Adjusts 'percent closed' to avoid Common Table Expression

--- sql/percent_closed_by_calendar_month.sql
+++ sql/percent_closed_by_calendar_month.sql
@@ -1,22 +1,38 @@
-WITH valid_wrs AS (
-    SELECT *
-    FROM afm.wrhwr
-    WHERE prob_type IS NOT NULL
-        AND prob_type != 'TEST (DO NOT USE)'
-        /* AND date_requested >= '01-01-2017' */
-),
-requested_and_closed AS (
-    SELECT wr_id,
-        CASE
-            WHEN status IN ('Com', 'Clo') THEN 1
-            ELSE 0
-        END AS is_closed,
-        dateadd(month, datediff(month, 0, date_requested), 0) AS calendar_month
-    FROM valid_wrs
-)
-SELECT CAST(calendar_month AS DATE),
-    CAST(SUM(is_closed) AS DECIMAL) / COUNT(*) AS percent_closed,
-    COUNT(*) AS wr_volume
-FROM requested_and_closed
-GROUP BY calendar_month
-ORDER BY calendar_month
+SELECT
+    calendar_month,
+    count_closed / count_opened AS pct_closed
+FROM
+    (
+        SELECT
+            calendar_month,
+            CAST(COUNT(*) AS DECIMAL) AS count_opened,
+            CAST(SUM(is_closed) AS DECIMAL) AS count_closed
+        FROM
+            (
+                SELECT
+                    CONVERT(
+                        VARCHAR(7),
+                        DateAdd(month, DateDiff(month, 0, date_requested), 0),
+                        120
+                    ) AS calendar_month,
+                    CASE
+                        WHEN status = 'Clo' THEN 1
+                        ELSE 0
+                    END AS 'is_closed'
+                FROM
+                    (
+                        SELECT
+                            *
+                        FROM
+                            DGS_Archibus.afm.wrhwr
+                        WHERE
+                            status NOT IN ('Can', 'R', 'Rej')
+                    ) wr_filtered
+                WHERE
+                    prob_type IS NOT NULL
+                    AND date_requested >= DateAdd(month, -6, DateAdd(year, -2, getDate()))
+                    AND date_requested < DateAdd(year, -2, getDate())
+            ) ungrouped
+        GROUP BY
+            calendar_month
+    ) grouped
