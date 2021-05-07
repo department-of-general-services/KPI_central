674c5a74b51f157813aed1395fd6122d60e254d3
James Trimarco <james.trimarco@gmail.com>
2021-05-07 15:17:00 -0400

    Moves scripts to /utils directory

---
+++ utils/sql_utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlalchemy as db
+import pyodbc
+from private.config import config
+from utils.general_utils import clean_report
+
+
+def mssql_engine(
+    username=config["username"], password=config["password"], server=config["server"]
+):
+    engine = db.create_engine(
+        f"mssql+pyodbc://{config['username']}:{config['password']}@{config['server']}/DGS_FasterWeb?driver=SQL+Server"
+    )
+    return engine
+
+
+def get_data_from_archibus(engine):
+    query = "SELECT * from DGS_Archibus.afm.wrhwr;"
+    faster_df = pd.read_sql(query, engine)
+    return faster_df
