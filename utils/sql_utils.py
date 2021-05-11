e781ff7aac5b16dda81eb0b4a38ead7472616454
James Trimarco <james.trimarco@gmail.com>
2021-05-11 10:42:08 -0400

    Points at Archibus, no FasterWeb

--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -2,19 +2,19 @@ import pandas as pd
 import sqlalchemy as db
 import pyodbc
 from private.config import config
-from utils.general_utils import clean_report
+# from utils.general_utils import clean_report
 
 
 def mssql_engine(
     username=config["username"], password=config["password"], server=config["server"]
 ):
     engine = db.create_engine(
-        f"mssql+pyodbc://{config['username']}:{config['password']}@{config['server']}/DGS_FasterWeb?driver=SQL+Server"
+        f"mssql+pyodbc://{config['username']}:{config['password']}@{config['server']}/DGS_Archibus?driver=SQL+Server"
     )
     return engine
 
 
 def get_data_from_archibus(engine):
-    query = "SELECT * from DGS_Archibus.afm.wrhwr;"
-    faster_df = pd.read_sql(query, engine)
-    return faster_df
+    query = "SELECT * from DGS_Archibus.afm.dash_benchmarks;"
+    archibus_df = pd.read_sql(query, engine)
+    return archibus_df
